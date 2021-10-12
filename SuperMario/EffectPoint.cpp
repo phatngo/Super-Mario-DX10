2aa9f2ff30bcdb2c060488ebb1be4d7924231f3a
Phat Ngo <ntp18799@gmail.com>
2021-10-12 21:38:49 +0700

    Refactor update and render class of Effect Point class

--- SuperMario/EffectPoint.cpp
+++ SuperMario/EffectPoint.cpp
@@ -8,14 +8,15 @@ EffectPoint::EffectPoint() {
 void EffectPoint::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
 	CGameObject::Update(dt, coObjects);
 	y += dy;
+	if (this->state == EFFECT_POINT_STATE_EXIST) {
+		if (start_Y - y >= POINT_FLYING_DY) {
+			SetState(EFFECT_POINT_STATE_NON_EXIST);
+		}
+	}
 }
 
 void EffectPoint::Render() {
-		if (this->state == EFFECT_POINT_STATE_EXIST) {
-			if (start_Y - y >= POINT_FLYING_DY) {
-				SetState(EFFECT_POINT_STATE_NON_EXIST);
-			}
-		}
+		
 	animation_set->at(POINT_GENERAL_ANI)->Render(x, y);
 }
 
